271e195da4d7dd3df80c147b5fddc0ce552e2c6e
mintu <mintu.giri1997@gmail.com>
2020-09-23 22:18:30 +0530

    Initial commit

---
+++ logutil/src/main/java/com/mintu/logutil/LogDebug.java
@@ -0,0 +1,17 @@
+package com.mintu.logutil;
+
+import android.util.Log;
+
+/**
+ * Created By Mintu Giri on 9/23/2020.
+ */
+public class LogDebug {
+
+    public static final String TAG = "LogDebug";
+
+    public static void log(String msg)
+    {
+        Log.d(TAG,msg);
+    }
+
+}
